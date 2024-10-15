fa77a70c7175c0e650647fd669a082c7d93b86cb
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-10-15 21:04:59 +0530

    cors policy set

--- backend/index.js
+++ backend/index.js
@@ -4,42 +4,46 @@ const connectDB = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const User = require('./models/user');
 const swagger = require('./swagger');
-const cors = require('cors')
-const profileMachinesRoutes = require('./routes/profileMachines')
-const profileBookingsRoutes= require('./routes/profileBookings')
-const imageRoutes=require('./routes/imageRoutes')
+const cors = require('cors');
+const profileMachinesRoutes = require('./routes/profileMachines');
+const profileBookingsRoutes = require('./routes/profileBookings');
+const imageRoutes = require('./routes/imageRoutes');
 
-
-  
 connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
-// middleware/upload.js
+
+// Middleware for file uploads
 const multer = require('multer');
-// Configure multer for file upload
 const storage = multer.memoryStorage(); // Store file in memory
 const upload = multer({ storage });
 
 app.use(express.json());
+
+// Update CORS options to allow multiple origins
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend URL
-  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: [
+    'http://localhost:5173', // Development URL
+    'https://agro-rent-oqfv.vercel.app/', // Production URL
+  ],
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 
 // Route to handle file upload
 app.post('/upload', upload.single('avatar'), async (req, res) => {
-    try {
-      const { name, mobile, password } = req.body;
-      const user = new User({ name, mobile, password, avatar: req.file.buffer });
-      await user.save();
-      res.status(201).send('File uploaded successfully');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+  try {
+    const { name, mobile, password } = req.body;
+    const user = new User({ name, mobile, password, avatar: req.file.buffer });
+    await user.save();
+    res.status(201).send('File uploaded successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 console.log("New Request");
 app.use("/api/user", require("./routes/user"));
 app.use("/api/owner", require("./routes/owner"));
@@ -57,6 +61,5 @@ app.use(swagger);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
